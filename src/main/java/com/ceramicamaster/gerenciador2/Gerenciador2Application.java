55d22a23c0182d4e9553fa3616d0b5ba6aa6ccd4
Murilo Rodrigues <murilooliveira10789@gmail.com>
2025-05-21 22:26:33 -0300

    First Commit

---
+++ src/main/java/com/ceramicamaster/gerenciador2/Gerenciador2Application.java
@@ -0,0 +1,13 @@
+package com.ceramicamaster.gerenciador2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gerenciador2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gerenciador2Application.class, args);
+	}
+
+}
